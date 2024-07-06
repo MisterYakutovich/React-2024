ecef3986be4bf0e0f67873bc6ae4e4897d2ba022
Pavel <pashtet.net5@gmail.com>
2024-07-06 21:50:45 +0300

    feat:add new component ErrorButton

---
+++ React-2024/src/components/ErrorButton/ErrorButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './ErrorButton.css';
+
+interface ErrorButtonState {
+  error: boolean;
+  hasError: boolean;
+}
+
+class ErrorButton extends React.Component<
+  Record<string, never>,
+  ErrorButtonState
+> {
+  state: ErrorButtonState = {
+    error: false,
+    hasError: false,
+  };
+
+  handleClick = () => {
+    this.setState({ error: true, hasError: true });
+    throw new Error('Oops, mistake!');
+  };
+
+  render() {
+    if (this.state.hasError) {
+      throw new Error('Oops, mistake!');
+    }
+    return (
+      <button className="error-button" onClick={this.handleClick}>
+        ErrorBoundary
+      </button>
+    );
+  }
+}
+export default ErrorButton;
