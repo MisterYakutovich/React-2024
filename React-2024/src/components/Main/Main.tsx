d6dafd67a80d8de3ec11e3e44b1f3026c6e4a52b
Pavel <pashtet.net5@gmail.com>
2024-07-07 11:12:34 +0300

    feat:add url_img for people;function extractIdFromUrl for comparisons url_api_person_id and id_jpg;add css for img

--- React-2024/src/components/Main/Main.tsx
+++ React-2024/src/components/Main/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Main.css';
 import { ArrSearchResult } from '../../Page';
+import { URL_PERSON, URL_EXTENSION } from '../../consts/api';
 
 interface SearchState {
   search: string;
@@ -14,6 +15,7 @@ export interface PeopleProps {
 interface PeopleArray {
   name: string;
   id: string;
+  url: string;
 }
 class Main extends React.Component<PeopleProps, SearchState> {
   state: SearchState = {
@@ -22,7 +24,7 @@ class Main extends React.Component<PeopleProps, SearchState> {
     isLoaded: false,
   };
   componentDidMount() {
-    fetch('https://swapi.dev/api/people/?page=4')
+    fetch('https://swapi.dev/api/people/?page=1')
       .then((res) => res.json())
       .then(
         (result) => {
@@ -38,6 +40,10 @@ class Main extends React.Component<PeopleProps, SearchState> {
         }
       );
   }
+  extractIdFromUrl(url: string): string {
+    const parts = url.split('/');
+    return parts[parts.length - 2];
+  }
   render() {
     const allItems: PeopleArray[] = this.state.items;
     const arrResultLocal: ArrSearchResult[] = this.props.localResult;
@@ -46,13 +52,21 @@ class Main extends React.Component<PeopleProps, SearchState> {
       <section className="section-main">
         <div className="container">
           {arrResultLocal.length == 0
-            ? allItems.map((i) => (
-                <div key={i.id} className="card">
-                  <p key={i.name} className="card_title">
-                    {i.name}
-                  </p>
-                </div>
-              ))
+            ? allItems.map((i) => {
+                const id = this.extractIdFromUrl(i.url);
+                return (
+                  <div key={i.id} className="card">
+                    <img
+                      className="card_img"
+                      src={`${URL_PERSON}${id}${URL_EXTENSION}`}
+                      alt={i.name}
+                    />
+                    <p key={i.name} className="card_title">
+                      {i.name}
+                    </p>
+                  </div>
+                );
+              })
             : arrResultLocal.map((i) => (
                 <div key={i.id} className="card">
                   <p key={i.name} className="card_title">
---
+++ React-2024/src/consts/api.ts
@@ -0,0 +1,4 @@
+const URL_IMG: string = 'https://starwars-visualguide.com/assets/img/';
+const URL_IMG_PEOPLE: string = 'characters/';
+export const URL_EXTENSION: string = '.jpg';
+export const URL_PERSON: string = URL_IMG + URL_IMG_PEOPLE;
