f256e5d9588582df1a9a0855558396e975ee3766
Pavel <pashtet.net5@gmail.com>
2024-07-15 00:22:03 +0300

    fix:add eslintignore

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,7 +24,6 @@ module.exports = {
     'eslint-plugin-react-compiler',
   ],
   rules: {
-   
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'comma-dangle': ['error', 'only-multiline'],
